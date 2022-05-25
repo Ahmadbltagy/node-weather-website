bdbc4ab830369bfc82204bc9dba5eb3df0f9ac5d
Ahmadbltagy <elbeltagyahmad@gmail.com>
2022-05-26 01:17:46 +0200

    Edit paragraph

--- public/js/app.js
+++ public/js/app.js
@@ -12,13 +12,13 @@ weatherForm.addEventListener("submit", (e) => {
   const location = search.value;
 
   messageOne.textContent = "Loading...";
-  messageTwo.textContent = "Loading...";
+  messageTwo.textContent = " ";
 
   getWeather(location).then((data) => {
     if (data.error) messageOne.textContent = data.error;
     else {
       messageOne.textContent = data.location;
-      messageTwo.textContent = data.forecast;
+      messageTwo.textContent = `It's currently ${data.forecast} degrees out`;
     }
   });
 });
